72ff2ce43cf673dca2816cf0c870a69fe98615bf
james59222 <james59222@gmail.com>
2023-07-25 20:03:01 -0500

    added project 3

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import image from "./pawsClawstails.PNG";
 import image2 from "./sbc2.jpg";
-
+import image3 from "./petGTP.PNG"
 function Portfolio() {
   return (
     <div className="Portfolio">
@@ -16,6 +16,8 @@ function Portfolio() {
         <br />
         <p>World Weather and Facts</p>
         <a href="https://cameronoberlies.github.io/Realtime-Weather-and-Country-Facts"><img src={image2} alt="Paws, Claws, and Tails" class="thumbnail" height="200" width="200" /></a>
+        <p>petGTP</p>
+        <a href="https://pet-matcher1-248c77335236.herokuapp.com/"><img src={image3} alt="petGTP" class="thumbnail" height="200" width="200" /></a>
 
     </div>
 
